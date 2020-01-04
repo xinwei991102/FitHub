9e04d2943979b782cdd290145e12959fcef3b349
xinwei991102 <ppfflop@gmail.com>
2020-01-04 22:38:15 +0800

    Xin Wei -added bottom navigation

--- app/src/main/java/com/example/fithub/MainActivity.kt
+++ app/src/main/java/com/example/fithub/MainActivity.kt
@@ -2,15 +2,47 @@ package com.example.fithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //TODO hi i am xinwei
-        //no i am jingwen
-
-        // ahha
+        val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
+                menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home-> {
+                    val fragment = HomeFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_exercise -> {
+                    val fragment = ExerciseFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.nav_profile -> {
+                    val fragment = ProfileFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                        .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+        bottom_navigation.setOnNavigationItemSelectedListener(navListener)
+        if (savedInstanceState == null) {
+            val fragment = HomeFragment()
+            supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+                .commit()
+        }
     }
+
+
+
 }
