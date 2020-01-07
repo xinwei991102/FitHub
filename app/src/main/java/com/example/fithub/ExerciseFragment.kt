58cabbbfcb4a644d44150d2391a84e0e7a24cc9e
xinwei991102 <ppfflop@gmail.com>
2020-01-07 13:37:28 +0800

    Merge branches 'heukei' and 'master' of https://github.com/xinwei991102/FitHub
    # Conflicts:
    #	app/src/main/res/values/strings.xml
    
    Customised app bar, added exercise activity with up button

--- app/src/main/java/com/example/fithub/ExerciseFragment.kt
+++ app/src/main/java/com/example/fithub/ExerciseFragment.kt
@@ -1,13 +1,24 @@
 package com.example.fithub
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_exercise.*
 
 
 class ExerciseFragment:Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.fragment_exercise, container, false)
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        buttonAerobic.setOnClickListener {
+            var intent = Intent(requireContext(), ExerciseActivity::class.java)
+            requireContext().startActivity(intent)
+        }
+    }
 }
