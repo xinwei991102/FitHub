628e08786cc3f3e27321af4491e491622cfba77a
xinwei991102 <ppfflop@gmail.com>
2020-01-07 14:50:30 +0800

    Added Youtube

--- app/src/main/java/com/example/fithub/ExerciseActivity.kt
+++ app/src/main/java/com/example/fithub/ExerciseActivity.kt
@@ -2,9 +2,13 @@ package com.example.fithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerSupportFragment
 
-class ExerciseActivity : AppCompatActivity() {
+class ExerciseActivity : AppCompatActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +19,35 @@ class ExerciseActivity : AppCompatActivity() {
         // Get a support ActionBar corresponding to this toolbar and enable the Up button
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val youTubePlayerFragment =
+            supportFragmentManager.findFragmentById(R.id.youtube_fragment) as YouTubePlayerSupportFragment;
+        youTubePlayerFragment.initialize("AIzaSyALK-JnsLHtjznQ_1ngrkc7W776f5EHTgM", this)
+
 
     }
 
+    override fun onInitializationSuccess(
+        provider: YouTubePlayer.Provider?,
+        player: YouTubePlayer?,
+        wasRestored: Boolean
+    ) {
+        if (player == null) return
+        if (wasRestored) {
+            player.play()
+        } else {
+            player.cueVideo("oHg5SJYRHA0")
+            player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT);
+        }
+    }
+
+    override fun onInitializationFailure(
+        provider: YouTubePlayer.Provider?,
+        result: YouTubeInitializationResult?
+    ) {
+        Log.d("Youtube Player", "Failed to initialize");
+    }
+
+    //Make up button behave as back button
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         when (item!!.itemId) {
             android.R.id.home -> {
