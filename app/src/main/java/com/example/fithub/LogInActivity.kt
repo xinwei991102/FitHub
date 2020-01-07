fb8f3d9834f59f5daa312e370fb71499d4a3eddf
SOH JING WEN <56525916+jingwensoh99@users.noreply.github.com>
2020-01-07 20:20:11 +0800

    jingwen

--- app/src/main/java/com/example/fithub/LogInActivity.kt
+++ app/src/main/java/com/example/fithub/LogInActivity.kt
@@ -2,11 +2,53 @@ package com.example.fithub
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        val email = editTextUsername.text.toString()
+        val password = editTextPassword.text.toString()
+
+        btnSignUp.setOnClickListener {
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            //Create a new user with email & password
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) {
+                        Log.d("Main", "Fail")
+                        return@addOnCompleteListener
+                    }
+                    //else if successful
+                    Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+                }
+
+        }
+
+        btnLogIn.setOnClickListener{
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("Log in", "signInWithEmail:success")
+                        val user = FirebaseAuth.getInstance().currentUser
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Log in", "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+        }
+
     }
 }
--- app/src/main/java/com/example/fithub/MainActivity.kt
+++ app/src/main/java/com/example/fithub/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.fithub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.annotation.NonNull
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseAuth.getInstance().signOut()
+        FirebaseAuth.AuthStateListener {
+            if(it.currentUser == null){
+                val intent = Intent(this, LogInActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
         val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
                 menuItem:MenuItem ->
             when (menuItem.itemId) {
@@ -41,8 +55,10 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment, fragment.javaClass.simpleName)
                 .commit()
         }
-    }
 
+
+
+    }
 
 
 }
