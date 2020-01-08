56eedd7b00053b139ef9b577fda5551420bb3cc5
xinwei991102 <ppfflop@gmail.com>
2020-01-09 00:07:11 +0800

    Merge branches 'heukei' and 'master' of https://github.com/xinwei991102/FitHub
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/fithub/ProfileFragment.kt
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/fithub/SignUpActivity.kt
+++ app/src/main/java/com/example/fithub/SignUpActivity.kt
@@ -59,8 +59,8 @@ class SignUpActivity : AppCompatActivity() {
         //Assign variable
         val name = username.text.toString()
         val gender= spinnerGender.selectedItem.toString()
-        val height = height.text.toString()
-        val weight = weight.text.toString()
+        val height = height.text.toString().toDouble()
+        val weight = weight.text.toString().toDouble()
 
 
         val id =ref.push().key
@@ -73,7 +73,7 @@ class SignUpActivity : AppCompatActivity() {
     }
 }
 
-class User(val name: String, val gender: String, val height: String, val weight: String)
+class User(val name: String, val gender: String, val height: Double, val weight: Double)
 
 
 
