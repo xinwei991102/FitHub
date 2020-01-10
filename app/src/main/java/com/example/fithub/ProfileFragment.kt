bc28ec8d6c7f8ffa9b27f7635df06b317fe2280c
hohk1 <hohk-wm17@student.tarc.edu.my>
2020-01-10 21:00:38 +0800

    heu kei

--- app/src/main/java/com/example/fithub/ProfileFragment.kt
+++ app/src/main/java/com/example/fithub/ProfileFragment.kt
@@ -2,6 +2,7 @@ package com.example.fithub
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,8 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_profile.*
+import java.net.URI
+import java.net.URL
 import kotlin.math.pow
 
 
